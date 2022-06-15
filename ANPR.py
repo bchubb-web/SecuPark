80a3f0a4cb1c6de002ae85654dd06c71abded3df
bchubb-web <billy@bchubb.co.uk>
2022-06-15 10:20:26 +0100

    progress
    created anpr handler, feed handler, and sql object

--- ANPR.py
+++ ANPR.py
@@ -1,42 +1,92 @@
 import cv2
+from datetime import datetime
 import mysql.connector
 import requests
+#from google.cloud import vision_vlp3beta1 as vision
+import numpy as np
+import time
+import json
 import re
 
 
 class feed_handler:
-    _status = str
-    _frame = any
+    _status: str
+    y_lower=[20, 35, 140]
+    y_upper = [50,255,255]
 
-    def __init__(self:object, cam:int ,number:str):
+    def __init__(self:object, cam:int ,number:str):# init the data for the camera, store as attributes
         self._CAMERA = cam
         self._ID = number
-        self._stream = cv2.VideoCapture(0)
+        self._stream = cv2.VideoCapture(cam)
 
-    def format_frame(self:object) -> any:
+    def format_frame(self:object) -> None:
 
         ret,frame = self._stream.read()
-        #resized_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+        height,width = frame.shape[:2]
+        frame = cv2.resize(frame,(800,int((height*800)/width)))
         self._frame = frame
 
+    def detect_motion(self:object) -> bool:
+        return True
+
+    def filter_frame(self:object) -> None:
+        hsv_frame = cv2.cvtColor(self._frame,cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_frame, np.array(self.y_lower), np.array(self.y_upper))
+        result = cv2.bitwise_and(self._frame,self._frame, mask = mask)
+        self._frame = result
+
+    def loop(self:object, fps:int) -> bool:
+        prev = 0
+        while True:
+            time_elapsed = time.time() - prev
+            self.format_frame()
+
+            if time_elapsed > 1./fps:
+                prev = time.time()
+                self.filter_frame()
+                self.output(self.detect_motion())
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return False
 
     def output(self:object, plate:bool) -> None:
         if plate: 
-            cv2.imshow("plate located", self._frame)
-            print("yyy")
+            cv2.imshow("plate found", self._frame)
+            
         else: cv2.imshow("no plate found", self._frame)
 
 
 class sql_handler:
-    db = mysql.connector.connect(user='root', password='', host='127.0.0.1',database='anpr_records')
-    cursor = db.cursor()
+
+    def __init__(self:object, details:dict) -> None:
+        self._user = details["user"]
+        self._password = details["password"]
+        self._host = details["host"]
+        self._db_name = details["database"]
+        
+    def connect(self:object) -> bool:
+        self._db = mysql.connector.connect(user=self._user, password=self._password, host=self._host, database=self._db_name)
+
+        if self._db:
+            self._cursor = self._db.cursor()
+            print("connection opened")
+            return True
+        else:
+            print("connection failed")
+            return False
+
+
 
     def execute(self:object, query:str, vals:any) -> bool:
         self.cursor.execute(query,vals)
+        self._db.commit()
+
+    def close(self) -> None:
+        self._db.close()
+        print("connection closed")
 
 
 class anpr_handler:
-    _endpoint = ""
+    _endpoint = "https://www."
     
     def __init__(self:object, frame:any, key:str):
         self._api_key = key
@@ -52,19 +102,24 @@ class anpr_handler:
 
 
 def main():
-    regex = "(^[A-Z]{2}[0-9]{2}\s?[A-Z]{3}$)|(^[A-Z][0-9]{1,3}[A-Z]{3}$)|(^[A-Z]{3}[0-9]{1,3}[A-Z]$)|(^[0-9]{1,4}[A-Z]{1,2}$)|(^[0-9]{1,3}[A-Z]{1,3}$)|(^[A-Z]{1,2}[0-9]{1,4}$)|(^[A-Z]{1,3}[0-9]{1,3}$)|(^[A-Z]{3}[0-9]{1,4}$)"
-    every_other = True
-    feed = feed_handler(0,"id")
 
-    while True:
-        feed.format_frame()
-        feed.output(True)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    feed._stream.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+    config = open("conf.json")
+    conf = json.load(config)
 
+    sql = sql_handler(conf["SQL"])
+    if sql.connect():
+
+    # regex = "(^[A-Z]{2}[0-9]{2}\s?[A-Z]{3}$)|(^[A-Z][0-9]{1,3}[A-Z]{3}$)|(^[A-Z]{3}[0-9]{1,3}[A-Z]$)|(^[0-9]{1,4}[A-Z]{1,2}$)|(^[0-9]{1,3}[A-Z]{1,3}$)|(^[A-Z]{1,2}[0-9]{1,4}$)|(^[A-Z]{1,3}[0-9]{1,3}$)|(^[A-Z]{3}[0-9]{1,4}$)"
+    
+        feed = feed_handler(conf["camera"],"id")
+        
+        fps = conf["fps"]
+
+        if not feed.loop(fps):
+            
+            feed._stream.release()
+            cv2.destroyAllWindows()
+            sql.close()
 
 if __name__ == '__main__':
     main()
