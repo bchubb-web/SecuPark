53309e5be2bfff524258633574450275908bea93
bchubb-web <billy@bchubb.co.uk>
2022-10-06 17:01:56 +0100

    installed sql

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {exec} = require('child_process');
 const express = require("express");
 const app = express();
 const path = require("path");
+const sql = require("mysql");
 
 
 app.use('/public/styles',express.static(__dirname+'/styles'));
@@ -15,8 +16,20 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname)+"\\index.html");
 });
 
-app.get('/admin', (req,res) => {
-    res.sendFile(path.join(__dirname)+"\\admin.html")
+app.get('/begin', (req,res) => {
+    res.sendFile(path.join(__dirname)+"\\begin.html")
+});
+
+
+
+app.post('/check/:locationID', (req,res) => {
+    var LID = req.params.locationID;
+    var frame = req.body.frame;
+    console.log(LID);
+    console.log(frame);
+    
+
+    res.send(200);
 });
 
 app.listen(3000,console.log('site live'));
